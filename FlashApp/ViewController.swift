027813a9fc76eef43bb2d142eb6cde79b55bb383
yeourain <yeourain@gmail.com>
2022-04-07 13:01:16 +1000

    Add some tags

--- FlashApp/ViewController.swift
+++ FlashApp/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+// 버튼 연결
+    @IBAction func buttonDidClick(_ sender: Any) {
+        label.text = "yeourain!"
+    }
+    
 }
 
